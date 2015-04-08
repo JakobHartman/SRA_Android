14a7052afeb6ebdac49450fa527e2d89e26e11b0
Chad Eddington <cdavideddington@gmail.com>
2015-04-07 18:29:11 -0600

    sample chart

--- app/src/main/java/com/special/StatsFragment.java
+++ app/src/main/java/com/special/StatsFragment.java
@@ -4,6 +4,8 @@ import org.rbdc.sra.Dashboard;
 import org.rbdc.sra.R;
 
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.ValueDependentColor;
+import com.jjoe64.graphview.series.BarGraphSeries;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 import com.special.menu.ResideMenu;
@@ -24,6 +26,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ScrollView;
 import android.widget.SeekBar;
+import android.widget.TextView;
 
 public class StatsFragment extends Fragment {
 
@@ -31,17 +34,18 @@ public class StatsFragment extends Fragment {
     private ResideMenu resideMenu;
     Button btn, btnCancel;
     Dialog dialog;
-    private View parentView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         final ScrollView v =  (ScrollView) inflater.inflate(R.layout.fragment_stats, container, false);
-        parentView = inflater.inflate(R.layout.fragment_stats, container, false);
+        View parentView = inflater.inflate(R.layout.fragment_stats, container, false);
+
+        TextView title = (TextView) parentView.findViewById(R.id.title);
 
 
         GraphView graph = (GraphView) parentView.findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> series = new LineGraphSeries<>(new DataPoint[] {
-                new DataPoint(0, 1),
+        BarGraphSeries<DataPoint> series = new BarGraphSeries<DataPoint>(new DataPoint[] {
+                new DataPoint(0, -1),
                 new DataPoint(1, 5),
                 new DataPoint(2, 3),
                 new DataPoint(3, 2),
@@ -49,7 +53,30 @@ public class StatsFragment extends Fragment {
         });
         graph.addSeries(series);
 
-        return v;
+// styling
+        series.setValueDependentColor(new ValueDependentColor<DataPoint>() {
+            @Override
+            public int get(DataPoint data) {
+                return Color.rgb((int) data.getX()*255/4, (int) Math.abs(data.getY()*255/6), 100);
+            }
+        });
+
+        series.setSpacing(50);
+
+// draw values on top
+        series.setDrawValuesOnTop(true);
+        series.setValuesOnTopColor(Color.RED);
+//series.setValuesOnTopSize(50);
+
+        title.setText("Sample Chart");
+
+
+        return parentView;
+    }
+
+    public void setUp() {
+
+
     }
 
     @Override
